03009d0cebee500ad1f31c298d964d3dd77de9f2
WinstonBrandon <winstonbrandon@outlook.com>
2022-07-27 05:14:28 +0200

    added initial set up for class MyAppHomepage

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter App', home: MyAppHomepage());
+  }
+}
+
+class MyAppHomepage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
