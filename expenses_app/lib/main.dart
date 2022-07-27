492040633902b48aecf658969f6966659d523c04
WinstonBrandon <winstonbrandon@outlook.com>
2022-07-27 05:21:19 +0200

    added appBar & body with 2 cards named CHART & LIST OF TX

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -5,13 +5,31 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(title: 'Flutter App', home: MyAppHomepage());
   }
 }
 
 class MyAppHomepage extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My Expenses App',
+        ),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: Text('CHART'),
+          ),
+          Card(
+            child: Text('LIST OF TX'),
+          ),
+        ],
+      ),
+    );
   }
 }
